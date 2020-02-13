8fc42740d9d94fc0e85a9852f04e3551c9267881
jongminshin <ace4463@gmail.com>
2020-02-13 14:31:54 +0900

    blockchain creating a block

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,28 @@
-interface Human{
-    name: string;
-    age: number;
-    gender: string;
+class Block{
+    public index: number;
+    public hash: string;
+    public previousHash: string;
+    public data: string;
+    public timestamp: number;
+    constructor(
+         index: number,
+         hash: string,
+         previousHash: string,
+         data: string,
+         timestamp: number
+         ){
+            this.index = index;
+            this.hash = hash;
+            this.previousHash = previousHash;
+            this.data = data;
+            this.timestamp = timestamp;
+        }
 }
 
+const genesisBlock:Block = new Block(0, "202020202000", "", "Hello", 123456)
 
-const person ={
-    name: "jongmin",
-    age: 29,
-    gender: "male"
-};
+let blockchain: [Block] = [genesisBlock];
 
-const sayHi = (person:Human): string =>{
-    return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!`
-}
-
-console.log(sayHi(person));
+console.log(blockchain);
 
 export {};
