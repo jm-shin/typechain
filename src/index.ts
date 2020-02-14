c7725da4e42ae54337ea9eb9bd098ca9d0955601
jongminshin <ace4463@gmail.com>
2020-02-14 13:36:20 +0900

    Conclusion

--- src/index.ts
+++ src/index.ts
@@ -55,6 +55,7 @@ const createNewBlock = (data:string): Block =>{
     const newTimestamp: number = getNewTimeStamp();
     const newHash: string = Block.calculateBlockHash(newIndex, previousBlock.hash, newTimestamp,data);
     const newBlock: Block = new Block(newIndex, newHash, previousBlock.hash, data, newTimestamp);
+    addBlock(newBlock);
     return newBlock;
 }
 
@@ -79,5 +80,11 @@ const addBlock = (candidateBlock: Block): void =>{
         blockchain.push(candidateBlock);
     }
 };
+
+createNewBlock("second block");
+createNewBlock("third block");
+createNewBlock("fourth block");
+
+console.log(blockchain);
 
 export {};
